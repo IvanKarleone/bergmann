33bbe7f7c1dd8cfc922330eea76827a943890ae7
Kirill Iliasov <kirill.ilyasov1997@gmail.com>
2024-09-27 12:22:06 +0200

    create mat-disallowed-button

---
+++ src/app/mat-disallowed-button/mat-disallowed-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'mat-disallowed-button',
+  standalone: true,
+  imports: [MatButton, MatTooltip],
+  templateUrl: './mat-disallowed-button.component.html',
+  styleUrl: './mat-disallowed-button.component.scss'
+})
+export class MatDisallowedButtonComponent {
+  readonly isDisallowed = input(false);
+  readonly disallowedTooltip = input('');
+
+  readonly clickEvent = output();
+}
